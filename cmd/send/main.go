635fb7f12d1b4f93b5b83413b96b1a63edfd9f08
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-10-29 14:40:33 +0700

    init

---
+++ cmd/send/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	_ "github.com/streadway/amqp"
+)
+
+func main() {
+	fmt.Println("Hello Client MQ")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/captain-corgi/rabbit-mq-example
+
+go 1.15
